7d79255c68b10d520367a0264dad868eb6d3b8a7
codemeow5 <codemeow@icloud.com>
2019-11-20 13:34:39 +0800

    Add MainPanel to MyCSharpBrowser project

---
+++ Samples/MyCSharpBrowser/MainPanel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TangramCLR;
+
+namespace MyCSharpBrowser
+{
+    public partial class MainPanel : UserControl
+    {
+        public MainPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnWhatIsNtp_Click(object sender, EventArgs e)
+        {
+            ChromeWebBrowser wb = Tangram.ActiveBrowser();
+            if (wb != null)
+            {
+                wb.OpenURL("https://www.chromium.org/user-experience/new-tab-page|", Disposition.NEW_POPUP, "", "");
+            }
+        }
+    }
+}
--- Samples/MyCSharpBrowser/Program.cs
+++ Samples/MyCSharpBrowser/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using TangramCLR;
@@ -17,7 +19,21 @@ namespace MyCSharpBrowser
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            LoadDefaultNTPXML();
             Application.Run(Tangram.Context);
+        }
+
+        static void LoadDefaultNTPXML()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var xmlResourceName = "MyCSharpBrowser.DefaultNTP.xml";
+            using (Stream stream = assembly.GetManifestResourceStream(xmlResourceName))
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    Tangram.NTPXml = reader.ReadToEnd();
+                }
+            }
         }
     }
 }
